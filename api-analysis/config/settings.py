dce5bf5f22098068d916cc4b7054897aea96dbb7
yeongjinHwang <yeongjin.gongjin@gmail.com>
2024-07-12 20:46:59 +0900

    ip, port num setup

---
+++ api-analysis/config/settings.py
@@ -0,0 +1,15 @@
+import socket
+
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = "127.0.0.1"
+    finally:
+        s.close()
+    return ip
+
+IP_NUM = get_local_ip()
+PORT_NUM = "9010"
