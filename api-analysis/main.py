4d1e8654eee9cbefbfddc87c636af6d6a9d7920d
yeongjinHwang <yeongjin.gongjin@gmail.com>
2024-07-16 10:51:21 +0900

    feature : TIER CALCULATION - 상위 100문제에 대한 점수로 반환 Related to: #10

--- api-analysis/main.py
+++ api-analysis/main.py
@@ -6,6 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from routers.root import router as root_router
 from routers.prob_generate import router as generate_router
 from routers.execute import router as execute_code_router
+from routers.tier import router as tier_calc_router
 
 from config.settings import IP_NUM, PORT_NUM
 
@@ -23,6 +24,7 @@ app.add_middleware(
 app.include_router(root_router)
 app.include_router(generate_router)
 app.include_router(execute_code_router)
+app.include_router(tier_calc_router)
 
 if __name__ == '__main__':
     command = f"uvicorn main:app --host {IP_NUM} --port {PORT_NUM} --workers 1"
